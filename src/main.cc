b008c84706ce1a0d6a4c633c60bcaad480363746
freetolga <freetolga@protonmail.com>
2025-06-05 09:05:42 +0300

    actual type safety POC

--- src/main.cc
+++ src/main.cc
@@ -4,6 +4,7 @@
 #include <SDL3/SDL_oldnames.h>
 #include <SDL3/SDL_render.h>
 #include <SDL3/SDL_video.h>
+#include <algorithm>
 #include <cstdint>
 #include <expected>
 #include <memory>
@@ -36,7 +37,7 @@ struct sdl_deleter {
 };
 
 struct SDLRendererWindow {
-public:
+private:
   std::unique_ptr<SDL_Renderer, sdl_deleter> renderer;
   std::unique_ptr<SDL_Window, sdl_deleter> window;
   SDLRendererWindow(const std::string_view title, int width, int height,
@@ -50,6 +51,17 @@ public:
     } else {
         renderer.reset(renderer_tmp);
         window.reset(window_tmp);
+    }
+  }
+public:
+  static std::expected<SDLRendererWindow,SDLErrors> create(const std::string_view title, int width, int height, SDL_WindowFlags window_flags) {
+    SDL_Renderer *renderer_tmp = nullptr;
+    SDL_Window *window_tmp = nullptr;
+    if (!SDL_CreateWindowAndRenderer(title.data(), width, height, window_flags,
+                                     &window_tmp, &renderer_tmp)) {
+        return std::unexpected(SDLErrors::SDLRendererCreateError);
+    } else {
+        return SDLRendererWindow(title,width,height,window_flags);
     }
   }
 };
@@ -78,14 +90,17 @@ public:
   auto draw_myself(SDL_Renderer *renderer) const -> bool{
     return set_my_color(renderer) && SDL_RenderFillRect(renderer, &rect);
   }
-  rect_demo(SDL_FRect rect, SDL_Color color) : rect(rect), color(color){}
+  rect_demo(SDL_FRect rect, SDL_Color color) : rect(rect = {.x = 0, .y = 0, .w = 0, .h = 0}), color(color = {.r = 0, .g = 0, .b = 0,.a = 0}){}
 };
 
 auto main(int argc, char **argv) -> int {
-    auto state = mte::SDLRendererWindow(window_title, window_width, window_height, SDL_WINDOW_RESIZABLE);
+    auto state = mte::SDLRendererWindow::create(window_title, window_width, window_height, SDL_WINDOW_RESIZABLE);
     SDL_SetRenderVSync(state.renderer.get(), 1);
     SDL_ShowWindow(state.window.get());
-    auto rect1 = rect_demo({.x = 10, .y = 10, .w = 10, .h = 10},{.r = 10, .g = 0, .b = 255, .a = 0});
+    std::array<rect_demo, (int) (window_width * window_height / 100)> rect_vector = {};
+    for(int idx = 0; idx < (window_width / 10) * (window_height / 10); ++idx) {
+        rect_vector[idx] = (rect_demo({.x = (float)(10 * idx), .y = (float)(std::max(10*idx, idx)), .w = 10, .h = 10}, {.r = 0, .g = 0,.b=0,.a=0}));
+    }
     for(SDL_Event e; e.type != SDL_EVENT_QUIT ;SDL_PollEvent(&e) ) {
         if(!SDL_SetRenderDrawColor(state.renderer.get(), 0, 0,0,0)) {
             std::printf("failed to clear renderer\n");
@@ -95,9 +110,11 @@ auto main(int argc, char **argv) -> int {
             std::printf("failed to clear renderer\n");
             continue;
         }
-        if(!rect1.draw_myself(state.renderer.get())) {
-            std::printf("failed to draw rect1\n");
-            continue;
+        for(auto &target_rect: rect_vector) {
+            if(!target_rect->draw_myself(state.renderer.get())) {
+                std::printf("failed to draw rect1\n");
+                continue;
+            }
         }
         if(!SDL_RenderPresent(state.renderer.get())) {
             std::printf("failed to present renderer\n");
